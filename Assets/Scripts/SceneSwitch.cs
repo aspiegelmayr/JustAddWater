d651ae9a16092412f33d67a7b6d7166dc73db10f
ash <anna.spiegelmayr@gmx.at>
2020-11-06 21:39:35 +0100

    basic room navigation

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void GoToKitchen()
+    {
+        SceneManager.LoadScene("Kitchen");
+    }
+
+    public void GoToBrokenPipeRoom()
+    {
+        SceneManager.LoadScene("BrokenPipeRoom");
+    }
+
+    public void GoToCafeteria()
+    {
+        SceneManager.LoadScene("Cafeteria");
+    }
+
+    public void GoToHammockRoom()
+    {
+        SceneManager.LoadScene("HammockRoom");
+    }
+
+    public void GoToMainRoom()
+    {
+        SceneManager.LoadScene("MainRoom");
+    }
+
+    public void GoToMedicineRoom()
+    {
+        SceneManager.LoadScene("MedicineRoom");
+    }
+
+    public void GoToStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
